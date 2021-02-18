5440a9993d9e6a11492c7db053c8bf4f7250e66b
Francesco Spinelli <francescospinelli2002@gmail.com>
2021-02-18 10:09:08 +0100

    Creazione oggetto base

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+/*
+Nome oggetto=Dati
+var Dati={
+    PopolazioneTot: document.getElementById(nomeId),
+    NumeroContagiati: document.getElementById(nomeId),
+    NumeroQuarantena: document.getElementById(nomeId),
+    NumeroSani: PopolazioneTot-NumeroContagiati,
+    TempoGuarigione: document.getElementById(nomeId),
+    NumeroGuariti: 0
+} 
+*/
