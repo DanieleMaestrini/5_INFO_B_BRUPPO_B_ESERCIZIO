056c0d87c951f52ace9b2d81372674dd38ceb10b
Francesco Spinelli <francescospinelli2002@gmail.com>
2021-02-22 17:41:43 +0100

    Aggiunta form in HTML

--- JS/script.js
+++ JS/script.js
@@ -4,8 +4,9 @@ var Dati={
     PopolazioneTot: document.getElementById(nomeId),
     NumeroContagiati: document.getElementById(nomeId),
     NumeroQuarantena: document.getElementById(nomeId),
-    NumeroSani: PopolazioneTot-NumeroContagiati,
+    NumeroSani: this.PopolazioneTot-this.NumeroContagiati,
     TempoGuarigione: document.getElementById(nomeId),
     NumeroGuariti: 0
 } 
 */
+
